98c417d748976322409fe1739543cdc0d6f3af7b
Mayur Kandalkar <40908920+Mkknowledge@users.noreply.github.com>
2024-06-23 12:50:11 +0530

    initial multi module roboticuw api

---
+++ fiscal-roboticuw-api/roboticuw-api/src/main/java/com/fiscalfreelife/fiscal_roboticuw_api/RoboticuwApiApplication.java
@@ -0,0 +1,13 @@
+package com.fiscalfreelife.fiscal_roboticuw_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboticuwApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboticuwApiApplication.class, args);
+	}
+
+}
---
+++ fiscal-roboticuw-api/roboticuw-api/src/test/java/com/fiscalfreelife/fiscal_roboticuw_api/RoboticuwApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiscalfreelife.fiscal_roboticuw_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboticuwApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
